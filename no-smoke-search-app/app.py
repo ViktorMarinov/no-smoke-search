3de56a69def35fb286dd5c12f3764485de2240a4
viktorgm <vm@hacker.works>
2019-02-08 12:40:57 +0200

    add inverted index to app and display results

--- no-smoke-search-app/app.py
+++ no-smoke-search-app/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, url_for, render_template
 from flask import flash, render_template, request, redirect
-
+from model.inverse_index import parse_query, find_matches
 app = Flask(__name__)
-
-# url_for('static', filename='style.css')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,17 +11,23 @@ def index():
 def search_results():
     search_query = request.args.get('search_query')
 
-    results = [1,2,3,5]
-    return render_template('results.html', search=search_query, results=results)
+    parsed_query = parse_query(search_query)
+    results = find_matches(parsed_query)
+
+    items = rows_to_items(results)
+
+    for result in rows_to_items(results):
+        print(result)
+
+    return render_template('results.html', search=search_query, items=items)
+
+
+def rows_to_items(results):
+    items = [row[1]for row in results.iterrows()]
+    for item in items:
+        item.tokens = set([t for t in item.tokens if len(t) > 1])
+    return items
+
 
 if __name__ == '__main__':
     app.run()
-
-
-
-        # results = search_string
-        # if not results:
-        #     flash('No results found!')
-        #     return redirect('/')
-        # else:
-        #     # display results
